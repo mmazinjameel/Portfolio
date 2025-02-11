5ac27c8be498fd1049303b23d1510815dbe6aede
mmazinjameel <mazinjameel987@gmail.com>
2025-02-11 23:10:57 +0530

    Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,13 +57,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    document.querySelectorAll('.js-scroll-trigger').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
+    // document.querySelectorAll('.js-scroll-trigger').forEach(anchor => {
+    //     anchor.addEventListener('click', function(e) {
+    //         e.preventDefault();
+    //         const targetId = this.getAttribute('href');
+    //         document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+    //     });
+    // });
 
 
 
